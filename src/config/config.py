70d6ea93ab45e2a619d213a345d174157c6c221d
Robin <rdoume@gmail.com>
2019-03-11 16:16:03 +0100

    Add dockerfile for generic image

---
+++ src/config/config.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3
+
+path_data='data/'
+langmodel_name='lid.176.ftz'
---
+++ src/scripts/download_ft_langdetect.sh
@@ -0,0 +1,2 @@
+wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz 
+cp lid.176.ftz data/
