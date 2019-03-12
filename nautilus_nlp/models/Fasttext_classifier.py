6eaaef0c2281d1a5220e007437b8ff71ef11cf64
Robin <rdoume@gmail.com>
2019-03-12 11:46:18 +0100

    Full interface Fasttext supervised classifier and begining unsupervised

--- nautilus_nlp/models/Fasttext_classifier.py
+++ nautilus_nlp/models/Fasttext_classifier.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 import fastText
+import multiprocessing
 
 
 class Fasttext_clf(BaseEstimator, ClassifierMixin):
@@ -11,14 +12,87 @@ class Fasttext_clf(BaseEstimator, ClassifierMixin):
             self.model = None
 
     def fit(self, X, y):
+        # Todo Build SKlearn compatible API for training
         return self
 
-    def predict(self, X):
-        r = self.model.predict(X)
+    def train(
+        self,
+        train_data,
+        lr=0.1,
+        dim=100,
+        ws=5,
+        epoch=5,
+        minCount=1,
+        minCountLabel=0,
+        minn=0,
+        maxn=0,
+        neg=5,
+        wordNgrams=1,
+        loss="softmax",
+        bucket=2000000,
+        thread=multiprocessing.cpu_count() - 1,
+        lrUpdateRate=100,
+        t=1e-4,
+        label="__label__",
+        verbose=2,
+        pretrainedVectors="",
+    ):
+        """
+    Train a supervised model and return a model object.
+    input must be a filepath. The input text does not need to be tokenized
+    as per the tokenize function, but it must be preprocessed and encoded
+    as UTF-8. You might want to consult standard preprocessing scripts such
+    as tokenizer.perl mentioned here: http://www.statmt.org/wmt07/baseline.html
+    The input file must must contain at least one label per line. For an
+    example consult the example datasets which are part of the fastText
+    repository such as the dataset pulled by classification-example.sh.
+    """
+        self.model = fastText.train_supervised(
+            input=train_data,
+            lr=lr,
+            dim=dim,
+            ws=ws,
+            epoch=epoch,
+            minCount=minCount,
+            minCountLabel=minCountLabel,
+            minn=minn,
+            maxn=maxn,
+            neg=neg,
+            wordNgrams=wordNgrams,
+            loss=loss,
+            bucket=bucket,
+            thread=thread,
+            lrUpdateRate=lrUpdateRate,
+            t=t,
+            label=label,
+            verbose=verbose,
+            pretrainedVectors=pretrainedVectors,
+        )
+
+    def predict(self, text, k=1, threshold=0.0, on_unicode_error="strict"):
+        """
+        Given a string, get a list of labels and a list of
+        corresponding probabilities. k controls the number
+        of returned labels. A choice of 5, will return the 5
+        most probable labels. By default this returns only
+        the most likely label and probability. threshold filters
+        the returned labels by a threshold on probability. A
+        choice of 0.5 will return labels with at least 0.5
+        probability. k and threshold will be applied together to
+        determine the returned labels.
+        This function assumes to be given
+        a single line of text. We split words on whitespace (space,
+        newline, tab, vertical tab) and the control characters carriage
+        return, formfeed and the null character.
+        If the model is not supervised, this function will throw a ValueError.
+        If given a list of strings, it will return a list of results as usually
+        received for a single line of text.
+        """
+        r = self.model.predict(text)
         return r
 
-    def predict_proba(self, X):
-        result_predict = self.model.predict(X)
+    def predict_proba(self, text):
+        result_predict = self.model.predict(text)
         if result_predict and len(result_predict) >= 2:
             probas = result_predict[1]
             if probas:
@@ -27,3 +101,38 @@ class Fasttext_clf(BaseEstimator, ClassifierMixin):
 
     def score(self, X, y=None):
         return sum(self.predict(X))
+
+    def quantize(
+        self,
+        input=None,
+        qout=False,
+        cutoff=0,
+        retrain=False,
+        epoch=None,
+        lr=None,
+        thread=None,
+        verbose=None,
+        dsub=2,
+        qnorm=False,
+    ):
+        self.model.quantize(
+            input, qout, cutoff, retrain, epoch, lr, thread, verbose, dsub, qnorm
+        )
+
+    def test(self, path, k=1):
+        """Evaluate supervised model using file given by path"""
+        return self.model.test(path, k)
+
+    def test_label(self, path, k=1, threshold=0.0):
+        """
+        Return the precision and recall score for each label.
+        The returned value is a dictionary, where the key is the label.
+        For example:
+        f.test_label(...)
+        {'__label__italian-cuisine' : {'precision' : 0.7, 'recall' : 0.74}}
+        """
+        return self.model.test_label(path, k, threshold)
+
+    def save_model(self, path):
+        """Save the model to the given path"""
+        self.model.save_model(path)
--- nautilus_nlp/models/Fasttext_embedding.py
+++ nautilus_nlp/models/Fasttext_embedding.py
@@ -24,3 +24,60 @@ class FasttextEmbedding(object):
             output: array
         """
         return self.model.get_sentence_vector(document)
+
+    def train(
+        self,
+        input,
+        model="skipgram",
+        lr=0.05,
+        dim=100,
+        ws=5,
+        epoch=5,
+        minCount=5,
+        minCountLabel=0,
+        minn=3,
+        maxn=6,
+        neg=5,
+        wordNgrams=1,
+        loss="ns",
+        bucket=2000000,
+        thread=multiprocessing.cpu_count() - 1,
+        lrUpdateRate=100,
+        t=1e-4,
+        label="__label__",
+        verbose=2,
+        pretrainedVectors="",
+    ):
+        """
+        Train an unsupervised model and return a model object.
+        input must be a filepath. The input text does not need to be tokenized
+        as per the tokenize function, but it must be preprocessed and encoded
+        as UTF-8. You might want to consult standard preprocessing scripts such
+        as tokenizer.perl mentioned here: http://www.statmt.org/wmt07/baseline.html
+        The input field must not contain any labels or use the specified label prefix
+        unless it is ok for those words to be ignored. For an example consult the
+        dataset pulled by the example script word-vector-example.sh, which is
+        part of the fastText repository.
+        """
+        self.model.train_unsupervised(
+            input=input,
+            model=model,
+            lr=lr,
+            dim=dim,
+            ws=ws,
+            epoch=epoch,
+            minCount=minCount,
+            minCountLabel=minCountLabel,
+            minn=minn,
+            maxn=maxn,
+            neg=neg,
+            wordNgrams=wordNgrams,
+            loss=loss,
+            bucket=bucket,
+            thread=thread,
+            lrUpdateRate=lrUpdateRate,
+            t=t,
+            label=label,
+            verbose=verbose,
+            pretrainedVectors=pretrainedVectors,
+        )
