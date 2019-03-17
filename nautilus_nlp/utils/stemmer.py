4979eacce61f161ebc268e76c2825bcdc0c8b262
hugovasselin <waaaouu@gmail.com>
2019-03-17 16:43:21 +0100

    add english and french lemmatization #11

--- nautilus_nlp/utils/stemmer.py
+++ nautilus_nlp/utils/stemmer.py
@@ -3,7 +3,7 @@ from nltk.stem.snowball import *
 
 def stem_tokens(tokens: list, lang: str ='english'):
     '''
-    Wrapper of NLTK's stemmers : http://www.nltk.org/howto/stem.html
+    Wrapper of NLTK's Snowball stemmers : http://www.nltk.org/howto/stem.html
 
     Args:
         tokens (list): list of tokens
@@ -21,7 +21,8 @@ def stem_tokens(tokens: list, lang: str ='english'):
         stemmer = eval(lang.capitalize()+'Stemmer()')
     else:
         raise ValueError("Langage not supported of mispelled")
-        
+    
+    # Make sure tokens are actually a list, and handle NaN. 
     if type(tokens) is list:
         return [stemmer.stem(token) for token in tokens]
     elif tokens is None:
