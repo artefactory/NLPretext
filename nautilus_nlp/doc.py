dea9beccf4c4a0d6e158091c1b76d13e3a0db7e4
Robin <rdoume@gmail.com>
2019-04-05 11:57:04 +0200

    Put the document abstraction into Nautilus. This Abstraction also solves #51

--- nautilus_nlp/doc.py
+++ nautilus_nlp/doc.py
@@ -1,8 +1,16 @@
 import functools
 import pkg_resources
 import nautilus_nlp
-lang_path = pkg_resources.resource_filename('nautilus_nlp.data', 'lang_identification.ftz')
-
+import spacy
+import textacy
+from collections import Counter
+import re
+import unicodedata
+import spacy
+import spacy.matcher
+import textacy
+import textacy.keyterms
+import textacy.text_utils
 
 class NautilusMissingModelException(Exception):
     """Raised when the requested model is missing"""
@@ -23,13 +31,46 @@ class Doc:
     _spacy_nlps: nested dictionary {lang: {model_id: model}} with loaded spacy language modules
     """
 
-    def __init__(self, raw, language=None, spacy_nlps=None,langdetect = None):
+    def __init__(
+        self,
+        raw,
+        language=None,
+        spacy_nlps=None,
+        langdetect=None,
+        sentiment_detect=None,
+    ):
         self.raw = raw
         self._spacy_nlps = spacy_nlps or dict()
         self._language = language
-        self._is_reliable_language = True if language else None
-        self._language_detector=langdetect
+        self._is_reliable_language = 1 if language else None
+        self._language_detector = langdetect
+        self._sentiment_detector = sentiment_detect
         self._text_stats = {}
+
+    @property
+    def language(self):
+        """
+        Provided or detected language of a text
+        >>> from nautilus_nlp.doc import Doc
+        >>> Doc('Test sentence for testing text').language
+        'en'
+        >>> Doc('Test sentence for testing text', language='en').language
+        'en'
+        >>> Doc('Test', hint_language='nl').language
+        'nl'
+        """
+
+        if not self._language:
+            if self._language_detector:
+                self._is_reliable_language, self._language = self._language_detector.detect_language(
+                    self.raw
+                )
+            else:
+                raise NautilusMissingModelException(
+                    "You must either provide a language for the document or an instance of LangDetector"
+                )
+
+        return self._language
 
     @property
     def _spacy_doc(self):
@@ -40,7 +81,7 @@ class Doc:
         >>> type(doc._spacy_doc)
         <class 'spacy.tokens.doc.Doc'>
         """
-        lang = self.language if self.is_reliable_language else self.hint_language
+        lang = self.language
 
         return self._load_spacy_doc(lang)
 
@@ -75,8 +116,42 @@ class Doc:
             )
         except IOError:
             raise NautilusMissingModelException(
-                f'Default model for language "{lang}" is not available.'
+                f'Default model for language "{lang}" is not available. You should try to run python -m spacy download {lang}_core_news_sm'
+            
             )
+
+    @property
+    def clean(self):
+        """
+        Cleaned text with sensible defaults.
+        >>> doc = Doc('“Please clean this piece… of text</b>„')
+        >>> doc.clean
+        '"Please clean this piece... of text"'
+        
+        Right now this is done here by a simple regex. Next step is to use the preprocessing functions
+        """
+
+        return self.clean_text()
+
+    @functools.lru_cache()
+    def clean_text(self, clean_dots=True, clean_quotes=True, clean_whitespace=True):
+        """
+        Clean text and normalise punctuation.
+        >>> doc = Doc('“Please clean this piece… of text„')
+        >>> doc.clean_text(False, False, False, False) == doc.raw
+        True
+        """
+        text = self.raw
+
+        if clean_dots:
+            text = re.sub(r"…", "...", text)
+        if clean_quotes:
+            text = re.sub(r"[`‘’‛⸂⸃⸌⸍⸜⸝]", "'", text)
+            text = re.sub(r"[„“]|(\'\')|(,,)", '"', text)
+        if clean_whitespace:
+            text = re.sub(r"\s+", " ", text).strip()
+
+        return text
 
     @property
     def entities(self):
@@ -97,11 +172,11 @@ class Doc:
         >>> doc.find_entities()
         [('Google', 'ORG')]
         """
-        lang = self.language if self.is_reliable_language else self.hint_language
+
         return list(
             {
                 (ent.text, ent.label_)
-                for ent in self._load_spacy_doc(lang, model_name).ents
+                for ent in self._load_spacy_doc(self.language, model_name).ents
             }
         )
 
@@ -137,7 +212,7 @@ class Doc:
         >>> doc.n_words
         5
         """
-        return len(self.n_words)
+        return len(self.words)
 
     @property
     def words(self):
@@ -185,29 +260,10 @@ class Doc:
         """
         Returns polarity score (-1 to 1) and a subjectivity score (0 to 1)
 
-        Currently only English, Dutch, French and Italian supported
-
-        >>> doc = Doc('Dit is een leuke zin.')
+        >>> doc = Doc('C'est trop cool !.')
         >>> doc.sentiment
-        (0.6, 0.9666666666666667)
+        (0.8, 0.9666666666666667)
         """
-
-        if self.language == "en":
-            from pattern.text.en import sentiment as sentiment_en
-
-            return sentiment_en(self.clean)
-        elif self.language == "nl":
-            from pattern.text.nl import sentiment as sentiment_nl
-
-            return sentiment_nl(self.clean)
-        elif self.language == "fr":
-            from pattern.text.fr import sentiment as sentiment_fr
-
-            return sentiment_fr(self.clean)
-        elif self.language == "it":
-            from pattern.text.it import sentiment as sentiment_it
-
-            return sentiment_it(self.clean)
 
         raise NautilusMissingModelException(f"No sentiment model for {self.language}")
 
@@ -229,7 +285,7 @@ class Doc:
         >>> doc.extract_keyterms(ranker='sgrank', ngrams=(1))
         [('Netherlands', 0.4020557546031188), ('capital', 0.29395103364295216), ('awesome', 0.18105611227666252), ('Amsterdam', 0.12293709947726655)]
         """
-        if self.nwords < 1:
+        if self.n_words < 1:
             return []
         rankers = ["textrank", "sgrank", "singlerank"]
         if ranker not in rankers:
@@ -249,146 +305,3 @@ class Doc:
         [('awesome', 0.32456160227748454), ('capital', 0.32456160227748454), ('Amsterdam', 0.17543839772251532)]
         """
         return self.extract_keyterms()
-
-    @property
-    def minhash(self):
-        """
-        A cheap way to compute a hash for finding similarity of docs
-        Source: https://ekzhu.github.io/datasketch/minhash.html
-        >>> doc = Doc('Sentence for computing the minhash')
-        >>> doc.minhash[:5]
-        [407326892, 814360600, 1099082245, 1176349439, 1735256]
-        """
-        return self.find_minhash()
-
-    @functools.lru_cache()
-    def find_minhash(self, num_perm=128):
-        words = self.words
-        doc_hash = MinHash(num_perm=num_perm)
-        for word, _ in words:
-            doc_hash.update(word.encode("utf8"))
-        return list(doc_hash.digest())
-
-    def similarity(self, other_doc, metric="jaccard", hash_method="minhash"):
-        """
-        Computes similarity for two documents.
-        Only minhash Jaccard similarity is implemented.
-        >>> doc1 = Doc('Sentence for computing the minhash')
-        >>> doc2 = Doc('Sentence for computing the similarity')
-        >>> doc1.similarity(doc2)
-        0.7265625
-        """
-        if hash_method == "minhash" and metric == "jaccard":
-            hash1 = MinHash(hashvalues=self.minhash)
-            hash2 = MinHash(hashvalues=other_doc.minhash)
-            return hash1.jaccard(hash2)
-        else:
-            raise NotImplementedError(
-                f"Metric/hash method combination {metric}"
-                f"/{hash_method} is not implemented as similarity metric"
-            )
-
-    @property
-    def word_vectors(self):
-        """
-        Returns word embeddings for the words in the document.
-        """
-        return self.generate_word_vectors()
-
-    @functools.lru_cache()
-    def generate_word_vectors(self, model_name=None):
-        """
-        Returns word embeddings for the words in the document.
-        The default spacy models don't have "true" word vectors
-        but only context-sensitive tensors that are within the document.
-
-        Returns:
-        A dictionary mapping words from the document to a dict with the
-        corresponding values of the following variables:
-
-        has vector: Does the token have a vector representation?
-        vector norm: The L2 norm of the token's vector (the square root of the
-                    sum of the values squared)
-        OOV: Out-of-vocabulary (This variable always gets the value True since
-                                there are no vectors included in the model)
-        vector: The vector representation of the word
-
-        >>> doc = Doc('Test sentence')
-        >>> doc.word_vectors['Test']['is_oov']
-        True
-        >>> len(doc.word_vectors['Test']['vector'])
-        96
-        >>> doc.word_vectors['Test']['vector_norm'] == doc.word_vectors['sentence']['vector_norm']
-        False
-        """
-        lang = self.language if self.is_reliable_language else self.hint_language
-        return {
-            token.text: {
-                "has_vector": token.has_vector,
-                "vector_norm": token.vector_norm,
-                "is_oov": token.is_oov,
-                "vector": token.vector.tolist(),
-            }
-            for token in self._load_spacy_doc(lang, model_name)
-        }
-
-    @property
-    def doc_vector(self):
-        """
-        Returns document embeddings based on the words in the document.
-
-        >>> import numpy
-        >>> numpy.array_equiv(Doc('a b').doc_vector, Doc('a b').doc_vector)
-        True
-        >>> numpy.array_equiv(Doc('a b').doc_vector, Doc('a a b').doc_vector)
-        False
-        """
-        return self.aggregate_word_vectors()
-
-    @functools.lru_cache()
-    def aggregate_word_vectors(
-        self, model_name=None, aggregation="mean", normalize=False, exclude_oov=False
-    ):
-        """
-        Returns document embeddings based on the words in the document.
-
-        >>> import numpy
-        >>> doc1 = Doc('a b')
-        >>> doc2 = Doc('a a b')
-        >>> numpy.array_equiv(doc1.aggregate_word_vectors(), doc1.aggregate_word_vectors())
-        True
-        >>> numpy.array_equiv(doc1.aggregate_word_vectors(), doc2.aggregate_word_vectors())
-        False
-        >>> numpy.array_equiv(doc1.aggregate_word_vectors(aggregation='mean'), doc2.aggregate_word_vectors(aggregation='sum'))
-        False
-        >>> numpy.array_equiv(doc1.aggregate_word_vectors(aggregation='mean'), doc2.aggregate_word_vectors(aggregation='var'))
-        False
-        >>> numpy.array_equiv(doc1.aggregate_word_vectors(aggregation='sum'), doc2.aggregate_word_vectors(aggregation='var'))
-        False
-        >>> doc = Doc('sentence with an out of vector word lsseofn')
-        >>> len(doc.aggregate_word_vectors())
-        96
-        >>> numpy.array_equiv(doc.aggregate_word_vectors(exclude_oov=False), doc.aggregate_word_vectors(exclude_oov=True))
-        False
-        """
-        lang = self.language if self.is_reliable_language else self.hint_language
-        tokens = [
-            token
-            for token in self._load_spacy_doc(lang, model_name)
-            if not exclude_oov or not token.is_oov
-        ]
-        vectors = [
-            token.vector / token.vector_norm if normalize else token.vector
-            for token in tokens
-        ]
-
-        if aggregation == "mean":
-            return numpy.mean(vectors, axis=0).tolist()
-        elif aggregation == "sum":
-            return numpy.sum(vectors, axis=0).tolist()
-        elif aggregation == "var":
-            return numpy.var(vectors, axis=0).tolist()
-        else:
-            raise NotImplementedError(
-                f"Aggregation method {aggregation} is not implemented."
-            )
