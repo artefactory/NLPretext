102fcaf41627440b51ffc13cda9e8ef7c1e9a4d1
Unknown <rdoume@gmail.com>
2019-04-05 16:42:16 +0200

    Script to download all spacy models

---
+++ download_spacy_models.sh
@@ -0,0 +1,5 @@
+python -m spacy download en_core_web_sm
+python -m spacy download de_core_news_sm 
+python -m spacy download fr_core_news_sm 
+python -m spacy download es_core_news_sm 
+python -m spacy download nl_core_news_sm
