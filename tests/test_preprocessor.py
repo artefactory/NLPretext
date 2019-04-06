1da5b530f294b136b7d79d60a953e325e4195395
Robin <rdoume@gmail.com>
2019-04-06 20:05:31 +0200

    Preprocessing tests

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -1,15 +1,21 @@
 import pytest
 import numpy as np
-from nautilus_nlp.utils.preprocess import (remove_multiple_spaces_and_strip_text, remove_accents)
+from nautilus_nlp.utils.preprocess import (
+    remove_multiple_spaces_and_strip_text,
+    remove_accents,
+)
 
 
-@pytest.mark.parametrize("input_str, expected_str", [
-    ("hello   world", "hello world"),
-    ("\n   hello world    ", "hello world"),
-    ("----- hello\tworld *****", "hello world"),
-    ("hello-world", "hello-world"),
-    ("hello - world", "hello world")
-])
+@pytest.mark.parametrize(
+    "input_str, expected_str",
+    [
+        ("hello   world", "hello world"),
+        ("\n   hello world    ", "hello world"),
+        ("----- hello\tworld *****", "hello world"),
+        ("hello-world", "hello-world"),
+        ("hello - world", "hello world"),
+    ],
+)
 def test_remove_multiple_spaces_and_strip_text(input_str, expected_str):
     result = remove_multiple_spaces_and_strip_text(input_str)
     np.testing.assert_string_equal(result, expected_str)
