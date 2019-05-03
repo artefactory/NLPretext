7ed11802186e91a965de2ddcd74aaf5dc247ac8b
Robin <rdoume@gmail.com>
2019-05-03 16:14:42 +0200

    Delete python2 support in compat.py

--- nautilus_nlp/utils/compat.py
+++ nautilus_nlp/utils/compat.py
@@ -7,43 +7,22 @@ is_windows = sys.platform.startswith("win")
 is_linux = sys.platform.startswith("linux")
 is_osx = sys.platform == "darwin"
 
-if is_python2:
-    import cPickle as pickle
-    from backports import csv
-    from itertools import izip as zip_
-    from urlparse import urljoin
 
-    range_ = xrange
+import csv
+import pickle
+from builtins import zip as zip_
+from urllib.parse import urljoin
 
-    bytes_ = str
-    unicode_ = unicode
-    string_types = (str, unicode)
-    int_types = (int, long)
-    chr_ = unichr
+range_ = range
 
-    def unicode_to_bytes(s, encoding="utf8", errors="strict"):
-        return s.encode(encoding=encoding, errors=errors)
+bytes_ = bytes
+unicode_ = str
+string_types = (bytes, str)
+int_types = (int,)
+chr_ = chr
 
-    def bytes_to_unicode(b, encoding="utf8", errors="strict"):
-        return unicode_(b, encoding=encoding, errors=errors)
+def unicode_to_bytes(s, encoding="utf8", errors="strict"):
+    return s.encode(encoding=encoding, errors=errors)
 
-
-else:
-    import csv
-    import pickle
-    from builtins import zip as zip_
-    from urllib.parse import urljoin
-
-    range_ = range
-
-    bytes_ = bytes
-    unicode_ = str
-    string_types = (bytes, str)
-    int_types = (int,)
-    chr_ = chr
-
-    def unicode_to_bytes(s, encoding="utf8", errors="strict"):
-        return s.encode(encoding=encoding, errors=errors)
-
-    def bytes_to_unicode(b, encoding="utf8", errors="strict"):
-        return b.decode(encoding=encoding, errors=errors)
+def bytes_to_unicode(b, encoding="utf8", errors="strict"):
+    return b.decode(encoding=encoding, errors=errors)
