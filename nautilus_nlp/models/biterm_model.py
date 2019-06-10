164474a132c4a53df83a6e5d76a6e980b216279a
pymousse <py.mousset@hotmail.fr>
2019-06-10 13:11:51 +0200

    change naming

--- nautilus_nlp/models/biterm_model.py
+++ nautilus_nlp/models/biterm_model.py
@@ -14,7 +14,7 @@ class BitermModel:
         :param data: a list of string, each string can be a document
         :param nb_topics: positive int
         :param nb_iteration: positive int
-        :param lang: str, language to remove the stop words, can be setup to None
+        :param lang: str, _language to remove the stop words, can be setup to None
         """
 
         self.is_int_positive(nb_topics)
@@ -25,51 +25,61 @@ class BitermModel:
         self.nb_topics = nb_topics
         self.nb_iteration = nb_iteration
         self.lang = lang
-        self.topics = None
-        self.btm = None
-        self.X = None
-        self.vocab = None
+        self._topics = None
+        self._btm = None
+        self._X = None
+        self._vocab = None
 
     @staticmethod
     def is_int_positive(number):
-        if type(number) != int:
+        """
+        Function to check if the input parameter is a integer and positive otherwise raise an error
+        :param number:
+        :return:
+        """
+        if not isinstance(number, int):
             raise ValueError("Parameter {} has to be an integer".format(number))
         if number < 1:
             raise ValueError("Parameter {} has to be positive".format(number))
 
     @staticmethod
     def is_list_of_string(data):
-        if type(data) != list:
+        """
+        Function to check if the input parameter is a list of strings otherwise raise an error
+        :param data:
+        :return:
+        """
+        if not isinstance(data, list):
             raise ValueError("{} has to be a list".format(data))
         if len(data) == 0:
             raise ValueError("{} is empty".format(data))
         for document in data:
-            if type(document) != str:
+            if not isinstance(document, str):
                 raise ValueError("All elements of {} have to be a string, problem with {}".format(data, document))
 
-    def get_clusters(self, nb_word_per_cluster):
+    def compute_topics(self, nb_word_per_cluster):
         vec = CountVectorizer(stop_words=self.lang)
-        self.X = vec.fit_transform(self.data).toarray()
-        self.vocab = np.array(vec.get_feature_names())
+        self._X = vec.fit_transform(self.data).toarray()
+        self._vocab = np.array(vec.get_feature_names())
 
-        biterms = vec_to_biterms(self.X)
-        self.btm = oBTM(num_topics=self.nb_topics, V=self.vocab)
-        self.topics = self.btm.fit_transform(biterms, iterations=self.nb_iteration)
+        biterms = vec_to_biterms(self._X)
+        self._btm = oBTM(num_topics=self.nb_topics, V=self._vocab)
+        self._topics = self._btm.fit_transform(biterms, iterations=self.nb_iteration)
 
-        results = topic_summuary(self.btm.phi_wz.T, self.X, self.vocab, nb_word_per_cluster, verbose=False)
+        results = topic_summuary(self._btm.phi_wz.T, self._X, self._vocab, nb_word_per_cluster, verbose=False)
 
         return results
 
     def get_document_topic(self, index):
-        if self.topics is None:
+        if self._topics is None:
             raise ValueError("Model needs to be trained first")
 
-        return self.topics[index].argmax()
+        return self._topics[index].argmax()
 
-    def save_pyLDAvis_plot(self, path_to_output='./plot.html'):
-        if self.topics is None or self.btm is None or self.X is None or self.vocab is None:
+    def save_pyLDAvis_plot(self, path_to_output='./biterm_pyLDAavis_plot.html'):
+        if self._topics is None or self._btm is None or self._X is None or self._vocab is None:
             raise ValueError("Model needs to be trained first")
 
-        vis = pyLDAvis.prepare(self.btm.phi_wz.T, self.topics, np.count_nonzero(self.X, axis=1), self.vocab,
-                               np.sum(self.X, axis=0))
+        vis = pyLDAvis.prepare(self._btm.phi_wz.T, self._topics, np.count_nonzero(self._X, axis=1), self._vocab,
+                               np.sum(self._X, axis=0))
         pyLDAvis.save_html(vis, path_to_output)
