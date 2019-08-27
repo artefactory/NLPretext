c3370af8a2b1f153e3bcb82d9d0758d2ba593ccb
Kais LARIBI <kais.laribi@artefact.com>
2019-08-27 14:51:15 +0200

    NMF short text

---
+++ nautilus_nlp/models/nmf_model.py
@@ -0,0 +1,99 @@
+
+import time
+import numpy as np
+from numpy.linalg import norm
+
+'''
+Topic Modeling via NMF
+'''
+
+class NMF(object):
+    def __init__(
+            self,
+            A, IW=[], IH=[],
+            n_topic=10, max_iter=100, max_err=1e-3,
+            rand_init=True):
+        '''
+        A = WH^T
+        '''
+        self.A = A
+        self.n_row = A.shape[0]
+        self.n_col = A.shape[1]
+
+        self.n_topic = n_topic
+        self.max_iter = max_iter
+        self.max_err = max_err
+
+        self.obj = []
+        if rand_init:
+            self.nmf_init_rand()
+        else:
+            self.nmf_init(IW, IH)
+        self.nmf_iter()
+
+    def nmf_init_rand(self):
+        self.W = np.random.random((self.n_row, self.n_topic))
+        self.H = np.random.random((self.n_col, self.n_topic))
+
+        for k in range(self.n_topic):
+            self.W[:, k] /= norm(self.W[:, k])
+
+    def nmf_init(self, IW, IH):
+        self.W = IW
+        self.H = IH
+
+        for k in range(self.n_topic):
+            self.W[:, k] /= norm(self.W[:, k])
+
+    def nmf_iter(self):
+        loss_old = 1e20
+        print('loop begin')
+        start_time = time.time()
+        for i in range(self.max_iter):
+            self.nmf_solver()
+            loss = self.nmf_loss()
+            self.obj.append(loss)
+
+            if loss_old - loss < self.max_err:
+                break
+            loss_old = loss
+            end_time = time.time()
+            print('Step={}, Loss={}, Time={}s'.format(i, loss, end_time - start_time))
+        print('loop end')
+
+    def nmf_solver(self):
+        '''
+        regular NMF without constraint.
+        Block Coordinate Decent
+        '''
+        epss = 1e-20
+
+        HtH = self.H.T.dot(self.H)
+        AH = self.A.dot(self.H)
+        for k in range(self.n_topic):
+            tmpW = self.W[:, k] * HtH[k, k] + AH[:, k] - np.dot(self.W, HtH[:, k])
+            self.W[:, k] = np.maximum(tmpW, epss)
+            self.W[:, k] /= norm(self.W[:, k]) + epss
+
+        WtW = self.W.T.dot(self.W)
+        AtW = self.A.T.dot(self.W)
+        for k in range(self.n_topic):
+            self.H[:, k] = self.H[:, k] * WtW[k, k] + AtW[:, k] - np.dot(self.H, WtW[:, k])
+            self.H[:, k] = np.maximum(self.H[:, k], epss)
+
+    def nmf_loss(self):
+        loss = norm(self.A - np.dot(self.W, np.transpose(self.H)), 'fro') ** 2 / 2.0
+        return loss
+
+    def get_loss(self):
+        return np.array(self.obj)
+
+    def get_lowrank_matrix(self):
+        return self.W, self.H
+
+    def get_decomposition_matrix(self):
+        return self.W, self.H
+
+    def save_format(self, Wfile='W.txt', Hfile='H.txt'):
+        np.savetxt(Wfile, self.W)
+        np.savetxt(Hfile, self.H)
--- nautilus_nlp/models/topic_modeling_short_text.py
+++ nautilus_nlp/models/topic_modeling_short_text.py
@@ -5,9 +5,8 @@ import numpy as np
 def data_preparation(text, vocab_min_count=1, vocab_max_size=10000):
     """
     This function expects a list of documents (sentences) and returns the needed data to
-    make topic modeling for short text using NMF.
+    make topic modeling for short text using NMF model.
     :return:
-
     """
 
     vocab = {}
@@ -37,11 +36,9 @@ def data_preparation(text, vocab_min_count=1, vocab_max_size=10000):
     return encoded_text_id, vocab_list, vocab_arr
 
 
-def train_model(model,n_docs, n_terms, docs, n_topics= 20, max_iter= 20, max_err=0.1, alpha = 0, beta=0):
+def train_model(model, encoded_text_id, vocab_list, n_topics= 20, max_iter= 20, max_err=0.1, alpha = 0, beta=0):
     """
     :param model:
-    :param n_docs:
-    :param n_terms:
     :param docs:
     :param n_topics:
     :param max_iter:
@@ -50,10 +47,14 @@ def train_model(model,n_docs, n_terms, docs, n_topics= 20, max_iter= 20, max_err
     :param beta:
     :return:
     """
+
+    n_docs = len(encoded_text_id)
+    n_terms = len(vocab_list)
+
     if model == 'nmf':
         dt_mat = np.zeros([n_terms, n_docs])
         for k in range(n_docs):
-            for j in docs[k]:
+            for j in encoded_text_id[k]:
                 dt_mat[j, k] += 1.0
         model = NMF(
             dt_mat,
@@ -63,6 +64,81 @@ def train_model(model,n_docs, n_terms, docs, n_topics= 20, max_iter= 20, max_err
 
     return model
 
+
+def show_dominant_topic(model, encoded_text_id, vocab_list, n_topKeyword =10):
+    """
+    Computes the PMi score for each topic and the topKeywords describing each of them.
+    :param model:
+    :param encoded_text_id:
+    :param vocab_list:
+    :return: topics = dictionnary with the topic number and its topkeywords
+             pmi_score = dictionnary with the topic number and its PMI score
+    """
+
+    dt_mat = __build_cooccurence_matrix(n_terms=len(vocab_list), encoded_text_id=encoded_text_id)
+    W,_ = model.get_decomposition_matrix()
+    n_topic = W.shape[1]
+    PMI_arr = []
+    for k in range(n_topic):
+        top_keywords_index = W[:, k].argsort()[::-1][:n_topKeyword]
+        PMI_arr.append(__calculate_PMI(dt_mat, top_keywords_index))
+
+    index = np.argsort(PMI_arr)
+    topics = {}
+    pmi_score = {}
+    for k in index:
+        words = []
+        for w in np.argsort(W[:, k])[::-1][:n_topKeyword]:
+            words.append(vocab_list[w])
+        # Complete the topic and the score dicts. Format {Topic_number: words or score}
+        topics[k] = words
+        pmi_score[k] = PMI_arr[k]
+
+    return topics, pmi_score
+
+def get_assigned_topics(model):
+    """
+    Assign the topic number to the sentences used when training the model
+    :param model: trained model
+    :return topics_list: list having the same length as the training text containing topics assigned to each sentence.
+    """
+    _, H = model.get_decomposition_matrix()
+    H_probs = H / H.sum(axis=1, keepdims=True)
+    topics_list = list(np.argmax(H_probs, axis=1) + 1)
+    return topics_list
+
+
+def __build_cooccurence_matrix(n_terms, encoded_text_id):
+    dt_mat = np.zeros([n_terms, n_terms])
+    for itm in encoded_text_id:
+        for kk in itm:
+            for jj in itm:
+                if kk != jj:
+                    dt_mat[int(kk), int(jj)] += 1.0
+    return dt_mat
+
+def __calculate_PMI(AA, topKeywordsIndex):
+    '''
+    Method to compute PMi score
+    Reference:
+    Short and Sparse Text Topic Modeling via Self-Aggregation
+    '''
+    D1 = np.sum(AA)
+    n_tp = len(topKeywordsIndex)
+    PMI = []
+    for index1 in topKeywordsIndex:
+        for index2 in topKeywordsIndex:
+            if index2 < index1:
+                if AA[index1, index2] == 0:
+                    PMI.append(0.0)
+                else:
+                    C1 = np.sum(AA[index1])
+                    C2 = np.sum(AA[index2])
+                    PMI.append(np.log(AA[index1,index2]*D1/C1/C2))
+    avg_PMI = 2.0*np.sum(PMI)/float(n_tp)/(float(n_tp)-1.0)
+
+    return avg_PMI
+
 ## TEST
 
 text = ['abs souscription ird abs collaborateur message bloquant finaliser merci selectionner un personne physique comme assurer principal',
@@ -71,14 +147,15 @@ text = ['abs souscription ird abs collaborateur message bloquant finaliser merci
 'abs souscription ird souscription un affaire nouveau abs collab arriver pas finaliser son contrat']
 encoded_text_id, vocab_list, vocab_arr = data_preparation(text)
 
-print(encoded_text_id)
+#print(encoded_text_id)
+#print(vocab_arr)
 
-print(vocab_arr)
+x = train_model('nmf', encoded_text_id, vocab_list, n_topics= 3)
+topics, pmi_score = show_dominant_topic(x, encoded_text_id, vocab_list, n_topKeyword =10)
 
-n_docs = len(encoded_text_id)
-n_terms = len(vocab_list)
-docs = encoded_text_id
-vocab = vocab_list
+print(topics)
 
-x = train_model('nmf', n_docs, n_terms, docs)
-W, H = x.get_decomposition_matrix()
+print(pmi_score)
+
+list_of_topics= get_assigned_topics(x)
+print(list_of_topics)
