d7e8f0fdffa446dd556ffe751f7612f7d16daeb8
Amale EL HAMRI <amale.elhamri@artefact.com>
2020-09-08 16:04:34 +0200

    added frequent words in keyword_extractor

--- nautilus_nlp/analysis/keyword_extractor.py
+++ nautilus_nlp/analysis/keyword_extractor.py
@@ -45,4 +45,31 @@ def extract_keywords(text, keyword, case_sensitive=True):
         processor.add_keywords_from_dict(keyword)
 
     return processor.extract_keywords(text)
+
+
+def frequent_words(list_words, ngrams_number=1, number_top_words=10):
+    """
+    Create n-grams for list of tokens
+
+    Parameters
+    ----------    
+    ngrams_number : int
     
+    number_top_words : int 
+        output dataframe length
+
+    Returns
+    -------
+    DataFrame
+        Dataframe with the entities and their frequencies.
+    """             
+    frequent = []
+    if ngrams_number == 1:
+        pass
+    elif ngrams_number >= 2:
+        list_words = create_ngrams(list_words, ngrams_number)
+    else:
+        raise ValueError("number of n-grams should be >= 1")
+    x = Counter(list_words)
+    frequent = x.most_common(number_top_words)
+    return frequent
