c143d61dfa719cdd61f492748f9c96cca3f94a8a
Amale EL HAMRI <amale.elhamri@artefact.com>
2020-09-24 16:53:06 +0200

    changing path in test biterm

--- tests/test_biterm.py
+++ tests/test_biterm.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from nautilus_nlp.models.biterm_model import BitermModel
+from nautilus_nlp.topic_modeling.biterm_model import BitermModel
 import pandas as pd
 import pytest
 
