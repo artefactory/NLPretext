f15eda487665ce803af5f26031a843b6bd8facfa
hugovasselin <waaaouu@gmail.com>
2020-10-16 18:38:11 +0200

    feat: add test for kw extractor

--- tests/test_keyword_extractor.py
+++ tests/test_keyword_extractor.py
@@ -1,5 +1,5 @@
 import pytest
-from nautilus_nlp.analysis.keyword_extractor import get_frequent_words
+from nautilus_nlp.analysis.keyword_extractor import get_frequent_words, extract_keywords
 
 
 @pytest.mark.parametrize(
@@ -18,3 +18,17 @@ def test_get_frequent_words(input_tokens, expected, ngrams_number, number_top_wo
 def test_get_frequent_words_output_lenght():
     input_tokens = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
     assert len(get_frequent_words(input_tokens, number_top_words=5)) == 5
+
+
+@pytest.mark.parametrize(
+    "input_text, keyword, case_sensitive, expected",
+    [
+        ("I eat an orange oranges at Orange", 'orange', False, ['orange', 'orange']),
+        ("I eat an orange oranges at Orange", 'orange', True, ['orange']),
+        ("I eat an orange oranges at Orange", {'orange': ['orange', 'oranges']}, False, ['orange', 'orange', 'orange']),
+        ("I eat an orange oranges at Orange", {'orange': ['orange', 'oranges']}, True, ['orange', 'orange']),
+        ("I eat an orange oranges at Orange", {'fruit': ['orange', 'oranges']}, True, ['fruit', 'fruit']),
+    ]
+    )
+def test_extract_keywords(input_text, keyword, case_sensitive, expected):
+    assert extract_keywords(input_text, keyword=keyword, case_sensitive=case_sensitive) == expected
