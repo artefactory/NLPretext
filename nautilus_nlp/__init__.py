f08668bc1d94132ff533256f0b88e292190a2602
Rafaelle AYGALENQ <rafaelle.aygalenq@artefact.com>
2021-01-26 10:54:57 +0100

    rename main folder and update readme

--- nautilus_nlp/__init__.py
+++ nautilus_nlp/__init__.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from preprocessing.preprocessor import Preprocessor
+from nautilus_nlp.preprocessor import Preprocessor
--- nautilus_nlp/social/preprocess.py
+++ nautilus_nlp/social/preprocess.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import emoji as _emoji
 from config import constants
-from preprocessing.classic.preprocess import normalize_whitespace
+from nautilus_nlp.classic.preprocess import normalize_whitespace
 
 
 def remove_mentions(text) -> str:
