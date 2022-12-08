f12fcab9dfbd631365b59c2272cbae44ab7a269f
Guillaume QUINQUET <guillaume.quinquet@artefact.com>
2022-12-08 14:06:58 +0100

    Fixed mypy errors

--- tests/test_textloader.py
+++ tests/test_textloader.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# mypy: disable-error-code="attr-defined"
 
 from unittest.mock import MagicMock, patch
 
